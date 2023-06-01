58fafe1646b09b2faccd148eef0dfc322915936b
Wayne <wayneck0138@gmail.com>
2023-06-01 12:49:31 +0800

    add highlight

--- controllers/search.js
+++ controllers/search.js
@@ -1,13 +1,13 @@
-import es from "../models/es.js";
+import es from '../models/es.js'
 
 export const autoComplete = async (req, res) => {
   try {
-    const { q } = req.body;
+    const { q } = req.body
 
-    const searchKey = q.split(" ");
+    const searchKey = q.split(' ')
 
     const data = await es.search({
-      index: "movies",
+      index: 'movies',
       body: {
         query: {
           regexp: {
@@ -15,46 +15,71 @@ export const autoComplete = async (req, res) => {
           },
         },
       },
-    });
-    const titles = data.hits.hits.map((ele) => ele._source.title);
+    })
+    const titles = data.hits.hits.map(ele => ele._source.title)
     const resData =
       searchKey.length > 1
-        ? titles.filter((ele) =>
+        ? titles.filter(ele =>
             ele.toLowerCase().includes(q.trim().toLowerCase())
           )
-        : titles;
-    return res.json(resData);
+        : titles
+    return res.json(resData)
   } catch (err) {
-    console.error(err);
-    return res.json(err);
+    console.error(err)
+    return res.json(err)
   }
-};
+}
 
 export const searchMoreInfo = async (req, res) => {
   try {
-    const { q } = req.body;
-    const searchKey = q.split(" ");
+    const { q } = req.body
+    const searchKey = q.split(' ')
     const data = await es.search({
-      index: "movies",
+      index: 'movies',
       body: {
         query: {
-          multi_match: {
-            query: q,
-            fields: ["title", "overview", "genres"],
+          bool: {
+            should: [
+              {
+                regexp: {
+                  title: `.*${searchKey[0].toLowerCase()}.*`,
+                },
+              },
+              {
+                regexp: {
+                  overview: `.*${searchKey[0].toLowerCase()}.*`,
+                },
+              },
+              {
+                wildcard: {
+                  'genres.keyword': `*${searchKey[0].toLowerCase()}*`,
+                },
+              },
+            ],
+          },
+        },
+        highlight: {
+          fields: {
+            title: {},
+            overview: {},
+            'genres.keyword': {},
           },
         },
       },
-    });
-    const info = data.hits.hits.map((ele) => ele._source);
+    })
+    console.log(data)
+    console.log(data.hits.hits[0].highlight)
+    const info = data.hits.hits.map(ele => ({
+      ...ele._source,
+      highlight: ele.highlight,
+    }))
     const resData =
       searchKey.length > 1
-        ? info.filter((ele) =>
-            ele.toLowerCase().includes(q.trim().toLowerCase())
-          )
-        : info;
-    return res.json(resData);
+        ? info.filter(ele => ele.toLowerCase().includes(q.trim().toLowerCase()))
+        : info
+    return res.json(resData)
   } catch (err) {
-    console.error(err);
-    return res.json(err);
+    console.error(err)
+    return res.json(err)
   }
-};
+}
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,57 +1,72 @@
 $(document).ready(function () {
-  $("#searchInput").on("input", function () {
-    var query = $(this).val();
-    $("#infop").empty();
+  $('#searchInput').on('input', function () {
+    var query = $(this).val()
+    $('#infop').empty()
     if (query) {
       $.ajax({
-        type: "POST",
-        url: "/autocomplete",
+        type: 'POST',
+        url: '/autocomplete',
         data: { q: query },
         success: function (data) {
           var suggestions = data.map(function (suggestion) {
-            return "<li>" + suggestion + "</li>";
-          });
-          $("#autocompleteList").html(suggestions.join(""));
+            return '<li>' + suggestion + '</li>'
+          })
+          $('#autocompleteList').html(suggestions.join(''))
         },
         error: function (error) {
-          console.log("no data");
+          console.log('no data')
         },
-      });
+      })
     } else {
-      $("#autocompleteList").empty();
+      $('#autocompleteList').empty()
     }
-  });
-});
+  })
+})
 $(document).ready(function () {
-  $("#searchButton").click(function () {
-    $("#autocompleteList").empty();
-    var query = $("#searchInput").val();
+  $('#searchButton').click(function () {
+    $('#autocompleteList').empty()
+    var query = $('#searchInput').val()
     if (query) {
       $.ajax({
-        type: "POST",
-        url: "/searchmore",
+        type: 'POST',
+        url: '/searchmore',
         data: { q: query },
         success: function (data) {
           // 處理搜索結果
-          console.log(data);
+          console.log(data)
           var suggestions = data.map(function (suggestion) {
+            var highlightedTitle =
+              suggestion.highlight && suggestion.highlight.title
+                ? suggestion.highlight.title[0]
+                : suggestion.title
+            var highlightedGenres =
+              suggestion.highlight && suggestion.highlight['genres.keyword']
+                ? suggestion.highlight['genres.keyword'][0]
+                : suggestion.genres.join(', ')
+            var highlightedOverview =
+              suggestion.highlight && suggestion.highlight.overview
+                ? suggestion.highlight.overview[0]
+                : suggestion.overview
             //- return '<div>' + suggestion.overview + '</div>';
             return `
-              <div class="movie">  
-                <div class="movie-title"> Title: ${suggestion.title} </div>
-                <div class="movie-rel_date"> Release Date: ${suggestion.release_date} </div>
-                <div class="movie-overview"> ${suggestion.overview} </div>
+            <div class="movie">  
+              <div class="movie-title"> Title: ${highlightedTitle} </div>
+              <div class="movie-rel_date"> 
+                Release Date: ${suggestion.release_date} &nbsp;&nbsp;&nbsp;&nbsp;  
+                Genres: ${highlightedGenres} 
               </div>
-              `;
-          });
-          $("#infop").html(suggestions.join(""));
+              <div class="movie-overview"> ${highlightedOverview} </div>
+            </div>
+            `
+          })
+          $('#infop').html(suggestions.join(''))
         },
         error: function (error) {
-          console.log("no data");
+          console.log('no data')
         },
-      });
+      })
     } else {
-      $("#infop").empty();
+      $('#infop').empty()
     }
-  });
-});
+  })
+})
