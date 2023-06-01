3f72c5271d980f6976a72a403158492305cee719
Wayne <wayneck0138@gmail.com>
2023-06-01 09:38:08 +0800

    add button

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-import express from "express";
-import dotenv from "dotenv";
-import es from "./models/es.js";
-import bodyParser from "body-parser";
+import express from 'express'
+import dotenv from 'dotenv'
+import es from './models/es.js'
+import bodyParser from 'body-parser'
 
-const app = express();
+const app = express()
 
-dotenv.config();
+dotenv.config()
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug')
+app.set('views', './views')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-app.post("/autocomplete", async (req, res) => {
+app.post('/autocomplete', async (req, res) => {
   try {
-    const { q } = req.body;
+    const { q } = req.body
 
-    const searchKey = q.split(" ");
+    const searchKey = q.split(' ')
 
     const data = await es.search({
-      index: "movies",
+      index: 'movies',
       body: {
         query: {
           regexp: {
@@ -31,22 +31,22 @@ app.post("/autocomplete", async (req, res) => {
           },
         },
       },
-    });
-    const titles = data.hits.hits.map((ele) => ele._source.title);
+    })
+    const titles = data.hits.hits.map(ele => ele._source.title)
     const resData =
       searchKey.length > 1
-        ? titles.filter((ele) =>
+        ? titles.filter(ele =>
             ele.toLowerCase().includes(q.trim().toLowerCase())
           )
-        : titles;
-    return res.json(resData);
+        : titles
+    return res.json(resData)
   } catch (err) {
-    console.error(err);
-    return res.json(err);
+    console.error(err)
+    return res.json(err)
   }
-});
+})
 
-app.get("/search", function (req, res) {
+app.get('/search', function (req, res) {
   es.search(
     {
       index: indexname,
@@ -54,17 +54,44 @@ app.get("/search", function (req, res) {
         query: {
           multi_match: {
             query: req.query.search,
-            fields: ["title", "overview"],
+            fields: ['title', 'overview'],
           },
         },
       },
     },
     function (error, response) {
-      res.json({ result: response });
+      res.json({ result: response })
     }
-  );
-});
+  )
+})
+
+app.post('/searchmore', async (req, res) => {
+  try {
+    const { q } = req.body
+    const searchKey = q.split(' ')
+    const data = await es.search({
+      index: 'movies',
+      body: {
+        query: {
+          multi_match: {
+            query: q,
+            fields: ['title', 'overview', 'genres'],
+          },
+        },
+      },
+    })
+    const info = data.hits.hits.map(ele => ele._source)
+    const resData =
+      searchKey.length > 1
+        ? info.filter(ele => ele.toLowerCase().includes(q.trim().toLowerCase()))
+        : info
+    return res.json(resData)
+  } catch (err) {
+    console.error(err)
+    return res.json(err)
+  }
+})
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  console.log('Server is running on port 3000')
+})
